a59e373ad8b8cec9add3933b5c288bba917481bd
Jesus Mendoza <ccredulidad@gmail.com>
2024-09-22 19:02:43 -0500

    Add choice.html route

--- assets/js/view/setViewTo.js
+++ assets/js/view/setViewTo.js
@@ -8,10 +8,12 @@ const optionsView = document.querySelector("#options-view");
 export const setViewTo = (view) => {
     switch (view) {
         case "choices":
+            window.history.pushState(null, "choice", "/I-Cant-Fucking-Decide-Comeback/choice.html");
             choiceView.classList.remove("invisible");
             optionsView.classList.add("invisible");
             break;
         case "options":
+            window.history.pushState(null, "home", "/I-Cant-Fucking-Decide-Comeback/");
             choiceView.classList.add("invisible");
             optionsView.classList.remove("invisible");
             break;
